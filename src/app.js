6d70ebfb7559886b2016f678b99b9e1d6a0a114a
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2024-04-13 22:20:14 +0530

    Development of eth-todo-list

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const App = {
+  load: async () => {
+    await App.loadWeb3();
+  },
+  loadWeb3: async () => {
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = window.ethereum;
+      web3 = new Web3(window.ethereum);
+    } else {
+      window.alert("Please connect to Metamask.");
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3.eth.sendTransaction({
+          /* ... */
+        });
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider;
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      });
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  },
+};
+
+$(document).ready(() => {
+  $(window).on("load", () => {
+    App.load();
+  });
+});
